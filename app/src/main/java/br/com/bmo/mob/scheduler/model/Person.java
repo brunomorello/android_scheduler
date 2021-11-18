c214936463e0ada9362cfc92da102e2b71beaa04
Bruno <brunomorello7@gmail.com>
2021-11-18 20:20:15 -0300

    feat: Implement toEquals and Hash

--- app/src/main/java/br/com/bmo/mob/scheduler/model/Person.java
+++ app/src/main/java/br/com/bmo/mob/scheduler/model/Person.java
@@ -2,7 +2,11 @@ package br.com.bmo.mob.scheduler.model;
 
 import androidx.annotation.NonNull;
 
-public class Person {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
     private final String nameStr;
     private final String emailStr;
     private final String phoneStr;
@@ -29,5 +33,18 @@ public class Person {
     @Override
     public String toString() {
         return nameStr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(nameStr, person.nameStr) && Objects.equals(emailStr, person.emailStr) && Objects.equals(phoneStr, person.phoneStr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameStr, emailStr, phoneStr);
     }
 }

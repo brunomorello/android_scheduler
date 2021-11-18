3164f52008068454abcc3a3d1726a69636954f98
Bruno <brunomorello7@gmail.com>
2021-11-18 19:40:33 -0300

    refactor: code quality

--- app/src/main/java/br/com/bmo/mob/scheduler/ui/activity/ListPeopleActivity.java
+++ app/src/main/java/br/com/bmo/mob/scheduler/ui/activity/ListPeopleActivity.java
@@ -2,7 +2,9 @@ package br.com.bmo.mob.scheduler.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -12,13 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.Arrays;
-import java.util.List;
-
 import br.com.bmo.mob.scheduler.R;
 import br.com.bmo.mob.scheduler.dao.PersonDAO;
 
 public class ListPeopleActivity extends AppCompatActivity {
+
+    private PersonDAO dao = new PersonDAO();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +37,22 @@ public class ListPeopleActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        PersonDAO dao = new PersonDAO();
+        setupListView();
+    }
 
+    private void setupListView() {
         ListView namesListView = findViewById(R.id.activity_main_names_list);
         namesListView.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 dao.getPeople()
         ));
+
+        namesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("posicao alunuo", "onItemClick: " + position);
+            }
+        });
     }
 }
